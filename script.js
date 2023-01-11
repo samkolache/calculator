4c5459df0ba2337db805941e09dc5d86909aa061
Sameer Kotecha <sameerzkotecha@gmail.com>
2023-01-11 16:45:45 -0600

    Create UI for calculator and start logic

---
+++ script.js
@@ -0,0 +1,118 @@
+let currentNum = ""
+let test = 0;
+const oneButton = document.getElementById("1")
+const twoButton = document.getElementById("2")
+const threeButton = document.getElementById("3")
+const fourButton = document.getElementById("4")
+const fiveButton = document.getElementById("5")
+const sixButton = document.getElementById("6")
+const sevenButton = document.getElementById("7")
+const eightButton = document.getElementById("8")
+const nineButton = document.getElementById("9")
+const addButton = document.getElementById("add");
+const clear = document.getElementById("clear");
+const display = document.getElementById("display")
+
+function doMath(num1, num2) {
+    first = num1;
+    second = num2;
+}
+
+
+oneButton.addEventListener("click", event => {
+    currentNum += "1"
+    display.innerText = currentNum;
+    doMath(currentNum);
+})
+
+twoButton.addEventListener("click", event => {
+    currentNum += "2"
+    display.innerText = currentNum;
+
+})
+
+threeButton.addEventListener("click", event => {
+    currentNum += "3"
+    display.innerText = currentNum;
+
+})
+
+fourButton.addEventListener("click", event => {
+    currentNum += "4"
+    display.innerText = currentNum;
+
+})
+
+fiveButton.addEventListener("click", event => {
+    currentNum += "5"
+    display.innerText = currentNum;
+
+})
+
+sixButton.addEventListener("click", event => {
+    currentNum += "6"
+    display.innerText = currentNum;
+
+})
+
+sevenButton.addEventListener("click", event => {
+    currentNum += "7"
+    display.innerText = currentNum;
+
+})
+
+eightButton.addEventListener("click", event => {
+    currentNum += "8"
+    display.innerText = currentNum;
+
+})
+
+nineButton.addEventListener("click", event => {
+    currentNum += "9"
+    display.innerText = currentNum;
+
+})
+
+clear.addEventListener("click", event => {
+    clearAll();
+})
+
+
+
+
+
+
+console.log(doMath())
+
+
+
+
+
+//created all math functions
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function divide(num1, num2) {
+    let ans =  num1 / num2;
+    return Math.round(ans * 10) / 10
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function negativeNumber(num) {
+    return -num;
+}
+
+function clearAll() {
+    currentNum = ""
+    display.innerHTML = 0;
+}
+
+
